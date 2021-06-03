b924ce90dc0968cb86ec1be1c8b7d710d7e5349d
Jiameng <jmfan@bu.edu>
2021-06-03 14:09:45 -0400

    new script

--- scripts/run_noisy_bg_cartpole_im84_dim1024_no_stacked_frames.sh
+++ scripts/run_noisy_bg_cartpole_im84_dim1024_no_stacked_frames.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --domain_name cartpole \
     --task_name swingup \
     --encoder_type rssm --work_dir ./log \
-    --action_repeat 8 --num_eval_episodes 8 \
+    --action_repeat 8 --num_eval_episodes 8 --kl_balance \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent DRIBO_sac --frame_stack 1 --encoder_feature_dim 1024 --save_model  \
     --seed 0 --critic_lr 1e-5 --actor_lr 1e-5 --eval_freq 10000 --batch_size 8 --num_train_steps 890000 &&
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --domain_name cartpole \
     --task_name swingup \
     --encoder_type rssm --work_dir ./log \
-    --action_repeat 8 --num_eval_episodes 8 \
+    --action_repeat 8 --num_eval_episodes 8 --kl_balance \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent DRIBO_sac --frame_stack 1 --encoder_feature_dim 1024 --save_model  \
     --seed 1 --critic_lr 1e-5 --actor_lr 1e-5 --eval_freq 10000 --batch_size 8 --num_train_steps 890000 &&
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --domain_name cartpole \
     --task_name swingup \
     --encoder_type rssm --work_dir ./log \
-    --action_repeat 8 --num_eval_episodes 8 \
+    --action_repeat 8 --num_eval_episodes 8 --kl_balance \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent DRIBO_sac --frame_stack 1 --encoder_feature_dim 1024 --save_model  \
     --seed 2 --critic_lr 1e-5 --actor_lr 1e-5 --eval_freq 10000 --batch_size 8 --num_train_steps 890000 &&
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --domain_name cartpole \
     --task_name swingup \
     --encoder_type rssm --work_dir ./log \
-    --action_repeat 8 --num_eval_episodes 8 \
+    --action_repeat 8 --num_eval_episodes 8 --kl_balance \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent DRIBO_sac --frame_stack 1 --encoder_feature_dim 1024 --save_model  \
     --seed 3 --critic_lr 1e-5 --actor_lr 1e-5 --eval_freq 10000 --batch_size 8 --num_train_steps 890000 &&
@@ -38,7 +38,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --domain_name cartpole \
     --task_name swingup \
     --encoder_type rssm --work_dir ./log \
-    --action_repeat 8 --num_eval_episodes 8 \
+    --action_repeat 8 --num_eval_episodes 8 --kl_balance \
     --pre_transform_image_size 100 --image_size 84 --noisy_bg \
     --agent DRIBO_sac --frame_stack 1 --encoder_feature_dim 1024 --save_model  \
     --seed 4 --critic_lr 1e-5 --actor_lr 1e-5 --eval_freq 10000 --batch_size 8 --num_train_steps 890000
